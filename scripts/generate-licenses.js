2221474c53bdb0c42a1c4184992964ac6e7e8d15
Augusto Seixas Corretor de Imóveis <joseadsseixas@gmail.com>
2026-01-04 08:19:36 -0300

    Add IPv4 family option to database connection

--- scripts/generate-licenses.js
+++ scripts/generate-licenses.js
@@ -15,6 +15,7 @@ if (!process.env.DATABASE_URL) {
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
+  family: 4
 });
 
 async function main() {
